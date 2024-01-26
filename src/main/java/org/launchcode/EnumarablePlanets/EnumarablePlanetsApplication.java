2d5ae06df04d002e05fcd76ee653922c17a9e0c9
el-sayit <tmanbros@gmail.com>
2024-01-26 00:06:11 -0600

    enum exercises done

---
+++ src/main/java/org/launchcode/EnumarablePlanets/EnumarablePlanetsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.EnumarablePlanets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumarablePlanetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnumarablePlanetsApplication.class, args);
+	}
+
+}
